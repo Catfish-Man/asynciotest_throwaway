9feb5a51c893d92b56dded451babc095e3330327
David Smith <david_smith@apple.com>
2025-02-12 20:30:25 +0000

    save wip

--- Sources/demo/demo.swift
+++ Sources/demo/demo.swift
@@ -1,28 +1,42 @@
 import SystemPackage
+import CSystem
+import Foundation
 
 @main
 struct MainApp {
+    @convention(c)
+    static func data_ready() {
+
+    }
+
     static func main() async throws {
         var ring = try IORing(queueDepth: 32)
         let parent = try FileDescriptor.open("/home/david/demo", .readOnly)
         let file = ring.registerFileSlots(count: 1).first!
         let buf = UnsafeMutableRawBufferPointer.allocate(byteCount: 65535, alignment: 0)
         let buffer = ring.registerBuffers(buf).first!
-        ring.prepare(
+        let efd = eventfd(0, Int32(EFD_NONBLOCK))
+
+        try ring.registerEventFD(FileDescriptor(rawValue: efd))
+        try ring.submit(
             linkedRequests:
-                IORequest(
-                    opening: "test.txt",
-                    in: parent,
-                    into: file,
-                    mode: .readOnly
-                ),
-            IORequest(
-                reading: file,
+            .opening(
+                "test.txt",
+                in: parent,
+                into: file,
+                mode: .readOnly
+            ),
+            .reading(
+                file,
                 into: buffer
             ),
-            IORequest(closing: file)
+            .closing(
+                file
+            )
         )
-        try ring.submitRequests()
-
+        let epollFD = epoll_create(1)
+        var epoll_evt = epoll_event() //see CFRunLoop.c for example epoll usage
+        epoll_evt.events = EPOLLIN | EPOLLET
+        epoll_ctl(epollFD, efd, EPOLL_CTL_ADD, &epoll_evt)
     }
 }

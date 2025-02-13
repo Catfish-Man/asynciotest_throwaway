54bdb16c964b882545804a4ca313ebe87b921284
David Smith <david_smith@apple.com>
2025-02-13 23:20:42 +0000

    Much closer to working, albeit with some hacks

--- Sources/demo/demo.swift
+++ Sources/demo/demo.swift
@@ -29,7 +29,7 @@ func handleCloseCompletion(_ completion: IOCompletion) {
     print(
         "Closed file with result \(String(cString: strerror(-completion.result))), completion \(completion)"
     )
-   // CSystem._exit(0)
+    // CSystem._exit(0)
 }
 
 func FILE_COUNT() -> UInt32 { 16 }
@@ -43,86 +43,95 @@ struct MainApp {
         let cwdbuf = getcwd(nil, 0)!
         print("cwd is \(String(cString: cwdbuf))")
         var ring = try IORing(queueDepth: FILE_COUNT() * 6)
+        let filenames = (0..<FILE_COUNT()).map { "testdatafile\($0).txt" }
         do {
             let files = ring.registerFileSlots(count: FILE_COUNT())
-            let slab = UnsafeMutableRawBufferPointer.allocate(byteCount: 32 * FILE_COUNT(), alignment: 0)
-                .evenlyChunked(in: FILE_COUNT())
+            let slab = UnsafeMutableRawBufferPointer.allocate(
+                byteCount: 32 * FILE_COUNT(), alignment: 0
+            )
+            .evenlyChunked(in: FILE_COUNT())
 
-            for (i, chunk) in zip(0 ..< FILE_COUNT(), slab) {
+            for (i, chunk) in zip(0..<FILE_COUNT(), slab) {
                 chunk.storeBytes(of: i, as: Int.self)
             }
-            let bPtrs = slab.lazy.map { 
+            let bPtrs = slab.lazy.map {
                 UnsafeMutableRawBufferPointer(rebasing: $0)
             }
             let buffers = ring.registerBuffers(bPtrs)
             let efd = eventfd(0, Int32(EFD_NONBLOCK))
 
-            for i in 0 ..< FILE_COUNT() {
-                ring.prepare(
-                    linkedRequests:
-                        .opening(
-                            "test\(i).txt",
-                            in: .currentWorkingDirectory,
-                            into: files[i],
-                            mode: .readWrite,
-                            options: .create,
-                            permissions: .ownerReadWrite
+            for i in 0..<FILE_COUNT() {
+                filenames[i].withCString { cfilename in
+                    ring.prepare(
+                        linkedRequests:
+                            .opening(
+                                cfilename,
+                                in: .currentWorkingDirectory,
+                                into: files[i],
+                                mode: .readWrite,
+                                options: .create,
+                                permissions: .ownerReadWrite
+                            ),
+                        .writing(
+                            buffers[i],
+                            into: files[i]
                         ),
-                    .writing(
-                        buffers[i],
-                        into: files[i]
-                    ),
-                    .closing(
-                        files[i]
+                        .closing(
+                            files[i]
+                        )
                     )
-                )
+                }
             }
 
-          //  try ring.registerEventFD(FileDescriptor(rawValue: efd))
+            try ring.registerEventFD(FileDescriptor(rawValue: efd))
             let readSrc = DispatchSource.makeReadSource(fileDescriptor: efd)
             var completedOperationCount = 0
 
             readSrc.setEventHandler(
                 handler: DispatchWorkItem {
                     while let completion = ring.tryConsumeCompletion() {
-                        switch completedOperationCount {
-                        case 0:
-                            handleOpenCompletion(completion)
-                        case 1:
-                            handleReadCompletion(completion, buffer: buffers[0])
-                        case 2:
-                            handleCloseCompletion(completion)
-                        default:
-                            fatalError()
-                        }
-
                         completedOperationCount += 1
+                        print("Got \(completion), number \(completedOperationCount)")
+                        // switch completedOperationCount {
+                        // case 0:
+                        //     handleOpenCompletion(completion)
+                        // case 1:
+                        //     handleReadCompletion(completion, buffer: buffers[0])
+                        // case 2:
+                        //     handleCloseCompletion(completion)
+                        // default:
+                        //     fatalError()
+                        // }
+
                     }
                 })
             readSrc.activate()
 
-            for i in 0 ..< FILE_COUNT() {
-                ring.prepare(
-                    linkedRequests:
-                        .opening(
-                            "test.txt",
-                            in: .currentWorkingDirectory,
-                            into: files[i],
-                            mode: .readOnly
+            for i in 0..<FILE_COUNT() {
+                filenames[i].withCString { cfilename in
+                    ring.prepare(
+                        linkedRequests:
+                            .opening(
+                                cfilename,
+                                in: .currentWorkingDirectory,
+                                into: files[i],
+                                mode: .readOnly
+                            ),
+                        .reading(
+                            files[i],
+                            into: buffers[i]
                         ),
-                    .reading(
-                        files[i],
-                        into: buffers[i]
-                    ),
-                    .closing(
-                        files[i]
+                        .closing(
+                            files[i]
+                        )
                     )
-                )
+                }
             }
 
             try ring.submitPreparedRequests()
 
             sleep(1000)
+            withExtendedLifetime(filenames) {}
         } catch (let openErr) {
             print(openErr)
         }

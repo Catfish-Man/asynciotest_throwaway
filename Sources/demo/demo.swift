343cb59c5d6e0ff660e8b611d2720878bf65496e
David Smith <david_smith@apple.com>
2025-02-13 22:20:51 +0000

    more wip

--- Sources/demo/demo.swift
+++ Sources/demo/demo.swift
@@ -38,12 +38,42 @@ struct MainApp {
         print("Running...")
         let cwdbuf = getcwd(nil, 0)!
         print("cwd is \(String(cString: cwdbuf))")
-        var ring = try IORing(queueDepth: 32)
+        var ring = try IORing(queueDepth: 1024)
         do {
-            let file = ring.registerFileSlots(count: 1).first!
-            let buf = UnsafeMutableRawBufferPointer.allocate(byteCount: 65535, alignment: 0)
-            let buffer = ring.registerBuffers(buf).first!
+            let files = ring.registerFileSlots(count: 512)
+            let slab = UnsafeMutableRawBufferPointer.allocate(byteCount: 32 * 512, alignment: 0)
+                .dropFirst(0)
+            withUnsafeTemporaryAllocation(of: UnsafeMutableRawBufferPointer.self, capacity: 512) {
+                ptrs in
+                for i in 1...512 {
+                    ptrs[i] = UnsafeMutableRawBufferPointer(rebasing: slab.dropFirst(32))
+                    ptrs[i].storeBytes(of: i, as: Int.self)
+                }
+                _ = ring.registerBuffers(ptrs)
+            }
+            let buffers = ring.registeredBuffers
             let efd = eventfd(0, Int32(EFD_NONBLOCK))
+
+            for i in 0..<512 {
+                ring.prepare(
+                    linkedRequests:
+                        .opening(
+                            "test\(i).txt",
+                            in: .currentWorkingDirectory,
+                            into: files[i],
+                            mode: .readWrite,
+                            options: .create
+                        ),
+                    .writing(
+                        buffers[i],
+                        into: files[i]
+                    ),
+                    .closing(
+                        files[i]
+                    )
+                )
+            }
+
             try ring.registerEventFD(FileDescriptor(rawValue: efd))
             let readSrc = DispatchSource.makeReadSource(fileDescriptor: efd)
             var completedOperationCount = 0
@@ -55,7 +85,7 @@ struct MainApp {
                         case 0:
                             handleOpenCompletion(completion)
                         case 1:
-                            handleReadCompletion(completion, buffer: buffer)
+                            handleReadCompletion(completion, buffer: buffers[0])
                         case 2:
                             handleCloseCompletion(completion)
                         default:
@@ -67,22 +97,26 @@ struct MainApp {
                 })
             readSrc.activate()
 
-            try ring.submit(
-                linkedRequests:
-                    .opening(
-                        "test.txt",
-                        in: .currentWorkingDirectory,
-                        into: file,
-                        mode: .readOnly
+            for i in 0..<512 {
+                ring.prepare(
+                    linkedRequests:
+                        .opening(
+                            "test.txt",
+                            in: .currentWorkingDirectory,
+                            into: files[i],
+                            mode: .readOnly
+                        ),
+                    .reading(
+                        files[i],
+                        into: buffers[i]
                     ),
-                .reading(
-                    file,
-                    into: buffer
-                ),
-                .closing(
-                    file
+                    .closing(
+                        files[i]
+                    )
                 )
-            )
+            }
+
+            try ring.submitPreparedRequests()
 
             sleep(1000)
         } catch (let openErr) {
